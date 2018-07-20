066963ccca1d26fe09e417c95a60a459ed6c8a49
Grant Hair <grant.hair@arnoldclark.com>
2018-07-20 19:15:12 +0100

    initial commit of files

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// Set constraints for the video stream
+var constraints = {
+    video:
+    {
+        facingMode: "environment"
+    }, audio: false
+};
+var track = null;
+var tracks = [];
+var buttonOuter = {};
+var button = {};
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+const input = document.querySelector('input[type="range"]');
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(
+            function (mediaStream) {
+                track = mediaStream.getVideoTracks()[0];
+                cameraView.srcObject = mediaStream;
+            })
+        .catch(function (error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    document.getElementById("camera--output").style.display = "block";
+    cameraOutput.classList.add("taken");
+    cameraTrigger.classList.add("camera--trigger__clicked")
+    //track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
